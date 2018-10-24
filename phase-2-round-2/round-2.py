911e64d3551897645acf7a677cd28bf57c4d4c66
Rudresh Pratap Singh <rudreshsingh999@gmail.com>
2018-10-25 01:40:51 +0530

    Update code of round2

--- phase-2-round-2/round-2.py
+++ phase-2-round-2/round-2.py
@@ -1,21 +1,21 @@
 from Arduino import Arduino
 import time
 import random
-
+file = open('output.txt','a')
 ar = [[1,4,3,2,5,8,7,6,9,10],[2,5,6,9,1,4,3,8,10,7],[7,9,2,5,1,8,6,3,10,4]]
 switch=[1,2,3,4,5,6,7,8,9,10]
-gates_led=[11,12,13,14,15,16,17,18,19,20]
+#gates_led=[11,12,13,14,15,16,17,18,19,20]
 gates = [21,22,23,24,25,26,27,28,29,30]
-gates_ir = [35,36,37,38,39,40,41,42,43,44]
-board.pinMode(31,"INPUT") # IR sensor pin1
-board.pinMode(32,"INPUT") # IR sensor pin2
-board.pinMode(33,"INPUT") # IR sensor pin3
+#gates_ir = [35,36,37,38,39,40,41,42,43,44]
+#board.pinMode(31,"INPUT") # IR sensor pin1
+#board.pinMode(32,"INPUT") # IR sensor pin2
+#board.pinMode(33,"INPUT") # IR sensor pin3
 board.pinMode(34,"INPUT") # Infinity stone
 penalty=5
-time =[None]*10   #ith element will store the time for which the ith gate need to be opened
+time =[1000]*10   #ith element will store the time for which the ith gate need to be opened
 for x in range(10):
 	board.Servos.attach(gates[x])
-r=random.choice([0,1,2])
+r = (int)(file.read())
 while True:
 	while (board.digitalRead(31)):
 		pass
@@ -28,8 +28,8 @@ while True:
 				if(i==0):
 					st=time.time()
 					i=1
-				board.digitalWrite(gates_led[ar[r][x]-1],"HIGH")
-			board.digitalWrite(gates_led[ar[r][x]-1],"LOW")
+				#board.digitalWrite(gates_led[ar[r][x]-1],"HIGH")
+			#board.digitalWrite(gates_led[ar[r][x]-1],"LOW")
 			if(i==1):
 				en=time.time()
 				time[ar[r][x]-1] = [en-st,ar[r][x]] # [time,gate_number]
@@ -38,21 +38,21 @@ while True:
 	for x in range(10):
 		if(time[x][0]==1000):
 			break
-		board.Servos.write(gates[time[x][1]-1],100)
+		board.Servos.write(gates[time[x][1]-1],100)   #opening of gate
 		flag=0
 		s1 = time.time();
         while((time.time()-s1)<(time[x][0]+0.1)):
-        	if(board.digitalRead(gates_ir[time[x][1]-1])==1):
+        	if(board.digitalRead(11)==1):
         		flag=1
-        board.Servos.write(gates[time[x][1]-1],0)
+        board.Servos.write(gates[time[x][1]-1],0)  #closing of gate
         if(flag==0):
         	board.Servos.write(gates[time[x][1]-1],100)
-        	while(board.digitalRead(gates_ir[time[x][1]-1])!=1):
+        	while(board.digitalRead(11)!=1):
         		pass
         	board.Servos.write(gates[time[x][1]-1],0)
         	penalty = penalty * 2
-	while(board.digitalRead(31)==0 & board.digitalRead(32)==0 & board.digitalRead(33)==0):
-		pass    
+	#while(board.digitalRead(31)==0 & board.digitalRead(32)==0 & board.digitalRead(33)==0):
+		#pass
 	main_end = time.time()
 	print (main_end - main_start)+penalty
 	exit()
