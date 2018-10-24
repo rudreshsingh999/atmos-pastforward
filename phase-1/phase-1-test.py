caa029f8e7c488afc7fdbfc3f426275d328786a6
Rudresh Pratap Singh <rudreshsingh999@gmail.com>
2018-10-24 19:46:23 +0530

    phase1: phase 1 test code

--- phase-1/phase-1-test.py
+++ phase-1/phase-1-test.py
@@ -1,12 +1,23 @@
 from Arduino import Arduino
 import time
 
-board = Arduino('9600') #plugged in via USB, serial com at rate 9600
+board = Arduino('9600',port="/dev/ttyACM0") #plugged in via USB, serial com at rate 9600
 board.pinMode(9, "OUTPUT")
 board.pinMode(3, "INPUT")
+file = open("output1.txt", 'w+')
+f=0
 while True:
     r=board.digitalRead(3)
     while (r==1):
+    	if(f==0):
+    		file.write("Switch is ON\n")
+    		f=1
         r=board.digitalRead(3)
         board.digitalWrite(9,"HIGH")
+        print(board.analogRead(3))
+        #file.write("this is line" + str(r))
     board.digitalWrite(9,"LOW")
+    if(f==1):
+    	file.write("Switch is OFF\n")
+    	f=0
+file.close()
--- phase-1/phase1_final_code.py
+++ phase-1/phase1_final_code.py
@@ -1,9 +1,16 @@
+from Arduino import Arduino
+import time
 import random
-
+board = Arduino('9600',port="/dev/ttyACM2")
 ar = [[1,4,3,2,5,8,7,6,9,10],[2,5,6,9,1,4,3,8,10,7],[7,9,2,5,1,8,6,3,10,4]]
-
+file = open('output.txt', 'a')
 switch=[1,2,3,4,5,6,7,8,9,10]
 gates=[11,12,13,14,15,16,17,18,19,20]
+
+for x in range(10):
+	board.Servos.attach(gates[x])
+	board.Servos.write(gates[x],90)
+
 r=random.choice([0,1,2])
 while True:
 	i=board.digitalRead(21)
@@ -11,11 +18,16 @@ while True:
 		i=board.digitalRead(21)
 		#delay
 	start = time.time()
+	file.write("switch ------ gate\n")
 	while(board.digitalRead(21)==0):
 		for x in range(10):
+			f=0
 			while(board.digitalRead(switch[x])==1):
-				board.digitalWrite(gates[ar[r][x]-1],"HIGH")
-			board.digitalWrite(gates[ar[r][x]-1],"LOW")
+				board.Servos.write(gates[x],0)
+				f=1
+			if(f == 1):
+				board.Servos.write(gates[x],90)
+				file.write(str(x+1)+"------"+str(ar[r][x])+"\n")
 	end = time.time()
-	print end-start
+	file.write("\n Time taken for phase1 : "+str(start-end))
 	exit()
